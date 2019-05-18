2d6965505fbd0e78cd1a5e8e319ee3c43b8d6c6f
MaratAkhmatnurov <upelsin.mapat@gmail.com>
2019-05-18 15:17:01 +0300

    start bot

---
+++ src/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"log"
+
+	"encoding/json"
+	"fmt"
+	"os"
+
+	tgbotapi "github.com/Syfaro/telegram-bot-api"
+)
+
+// Configuration with bot token
+type Configuration struct {
+	TelegramBotToken string
+}
+
+var (
+	telegramBotToken string
+)
+
+func init() {
+
+	file, _ := os.Open("../configs/secrets.json")
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	configuration := Configuration{}
+	err := decoder.Decode(&configuration)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	telegramBotToken = configuration.TelegramBotToken
+}
+
+func main() {
+
+	bot, err := tgbotapi.NewBotAPI(telegramBotToken)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	// u - структура с конфигом для получения апдейтов
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	// используя конфиг u создаем канал в который будут прилетать новые сообщения
+	updates, err := bot.GetUpdatesChan(u)
+
+	// в канал updates прилетают структуры типа Update
+	// вычитываем их и обрабатываем
+	for update := range updates {
+		// универсальный ответ на любое сообщение
+		reply := "Не знаю что сказать"
+		if update.Message == nil {
+			continue
+		}
+
+		// логируем от кого какое сообщение пришло
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		// свитч на обработку комманд
+		// комманда - сообщение, начинающееся с "/"
+		switch update.Message.Command() {
+		case "start":
+			reply = "Привет. Я телеграм-бот"
+		case "hello":
+			reply = "world"
+		}
+
+		// создаем ответное сообщение
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
+		// отправляем
+		bot.Send(msg)
+	}
+}
